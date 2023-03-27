ec24d4c7aa8d36b211bbe2b9d1d586680224ba57
Konda Sandeep <82932586+SandeepK1729@users.noreply.github.com>
2023-03-27 18:53:45 +0000

    Done with DatasetView and ModelView Component

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Datasets from './components/DatasetsView';
+import Models from './components/ModelView';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, World!</h1>
+      <Models/>
+    </div>
+  );
+}
+
+export default App;
